0c8411b728ae7be06f32c4ed7a5fc0f208665935
Divyanshi <126316406+DivyanshiSingh00@users.noreply.github.com>
2025-04-14 16:13:04 +0530

    Create 509. Fibonacci Number.cpp

---
+++ Step 1 : Learn the basics/509. Fibonacci Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        return rec(n, dp);
+    }
+    int rec(int n, vector<int>& dp) {
+        if (n == 0 || n == 1) return n;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = rec(n - 1, dp) + rec(n - 2, dp);
+    }
+};
